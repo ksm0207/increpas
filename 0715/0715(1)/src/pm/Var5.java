6bb5c082b12a2af14cb6ad97989f219848b3cd43
ksm0207 <ksm03071@naver.com>
2021-07-15 17:04:38 +0900

    - 인크레파스 자바 2일차

---
+++ 0715/0715(1)/src/pm/Var5.java
@@ -0,0 +1,12 @@
+package pm;
+
+public class Var5 {
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 7;
+		int c = a + b;
+		
+		System.out.println(c);
+	}
+}
---
+++ 0715/0715/src/pm/Var5.java
@@ -0,0 +1,18 @@
+package pm;
+
+public class Var5 {
+	public static void main(String[] args) {
+		
+		
+		int a = 10;
+		int b = 7;
+		
+		int c = a + b;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
