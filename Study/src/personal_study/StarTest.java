b5846e52c9355a084caeba4da6fa71634053bafb
ksm0207 <ksm03071@naver.com>
2021-07-19 20:54:28 +0900

    - 개인공부 별찍기 도전중

---
+++ Study/src/personal_study/StarTest.java
@@ -0,0 +1,20 @@
+package personal_study;
+
+public class StarTest {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0 ; i < 4 ; i ++) {
+			
+			for (int j = 0 ; j < (3 - i) ; j ++) {
+				System.out.print(" ");
+			}
+			
+			for (int k = 0; k < 2 * i + 1 ; k ++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+		
+	}
+}
--- increpas0719/src/pm/Ex8_MultiFor.java
+++ increpas0719/src/pm/Ex8_MultiFor.java
@@ -10,11 +10,24 @@ public class Ex8_MultiFor {
 		// * * * *
 		// * * * * *
 		
+		// 상단 
 		for (int i = 0 ; i < 5 ; i ++) {
 			for (int j = 0 ; j <= i ; j ++) {
-				System.out.print("* ");
+				System.out.print("* " + " ");
+			}
+			System.out.println();
+		}// end of for loop(1)
+		
+		// 하단
+		for (int i = 5 ; i > 0 ; i --) {
+			
+			for (int j = 1 ; j <= i ; j ++) {
+				System.out.print("* " + " ");
 			}
 			System.out.println();
 		}
+		
+		
+		
 	}
 }
