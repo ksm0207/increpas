6a8b2f8d53ffb0821b4b35e95ce14bdcde76a2a1
ksm0207 <ksm03071@naver.com>
2021-07-19 13:14:50 +0900

    개인공부 String [] args 활용해보기(3)
    - 다른 방법으로 풀이

--- Study/src/personal_study/Args.java
+++ Study/src/personal_study/Args.java
@@ -4,5 +4,41 @@ import java.util.Scanner;
 
 public class Args {
 	
+	public boolean check(String [] value) {
+		boolean b = true;
+		
+		for (int i = 0 ; i < value.length ; i ++ ) {
+	
+			if (Character.isUpperCase(value[i].charAt(0))) {
+				return b;
+			}else if (Character.isDigit(value[i].charAt(0))) {
+				return b;
+			}else if (Character.isLowerCase(value[i].charAt(0))) {
+				return b;
+			}else {
+				b = false;
+			}
+		}
+		return b;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		Args arg = new Args();
+		args = new String [1];
+		
+		System.out.print("입력 : ");
+		for (int i = 0 ; i < args.length; i ++) {
+			args[i] = scan.next();
+			arg.check(args);
+		}
+		
+		for (int i = 0; i < args.length ; i ++) {
+			System.out.println("'첫'대소문자 or 숫자 체크");
+			System.out.println("입력 한 값 : " + args[i]);
+			System.out.println("결과 : " + arg.check(args));
+		}
+	}
 	
 }
