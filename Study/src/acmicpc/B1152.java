0a0ace93451d77aab9ad5f0b50388a0350906586
ksm0207 <ksm03071@naver.com>
2021-07-20 20:13:36 +0900

    - 백준 1152 풀이
    - 단어의 개수

--- Study/src/acmicpc/B1152.java
+++ Study/src/acmicpc/B1152.java
@@ -1,10 +1,21 @@
 package acmicpc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class B1152 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		// 1. 단어의 개수
-		// 2. 조건 : 영어 대소문자와 띄어스기만으로 이루어진다
-		// 3. 출력 : 몇개의 단어가 있는지
+		// 1. 단어의 개수 - 1152번
+		// 2. 조건 : 영어 대소문자와 띄어쓰기만으로 이루어진다
+		// 3. 출력 : 몇개의 단어가 있는지 체크할 것
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		System.out.println(st.countTokens());
+		
 	}
 }
