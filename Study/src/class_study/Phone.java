222f5f7d6e161712344d0d9dae27842cc0bcef91
ksm0207 <ksm03071@naver.com>
2021-07-20 09:04:33 +0900

    - 개인공부 클래스 정리

---
+++ Study/src/class_study/Phone.java
@@ -0,0 +1,29 @@
+package class_study;
+
+public class Phone {
+	
+	// 멤버 필드 정의
+	String color;
+	int volume;
+	
+	public void setColor(String col) {
+		color = col;
+		System.out.println("색상 : "+color);
+	}
+	
+	public void volumeUp(int up) {
+		volume += up;
+		System.out.println("볼륨 업 : "+volume);
+	}
+	public void volumeDown(int down) {
+		volume -= down;
+		System.out.println("볼륨 다운 : "+volume);
+	}
+	
+	public static void main(String[] args) {
+		Phone phone = new Phone();
+		phone.setColor("Yellow");
+		phone.volumeUp(100);
+		phone.volumeDown(43);
+	}
+}
