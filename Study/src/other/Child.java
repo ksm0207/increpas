578668230579934b52451006bfbecdfac5469196
ksm0207 <ksm03071@naver.com>
2021-07-21 07:32:05 +0900

    - 개인공부 protected 사용법

---
+++ Study/src/other/Child.java
@@ -0,0 +1,12 @@
+package other;
+
+import test.Sample;
+
+public class Child extends Sample {
+	
+	public static void main(String[] args) {
+		Child c = new Child();
+		
+		System.out.println(c.a);
+	}
+}
---
+++ Study/src/test/Sample.java
@@ -0,0 +1,6 @@
+package test;
+
+public class Sample {
+	
+	protected String a = "다른 패키지에 존재하는 자식 클래스까지만 허용";
+}
