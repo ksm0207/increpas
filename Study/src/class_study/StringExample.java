15046c7df3c902e6f4aa8a5493e638c80e995d05
ksm0207 <ksm03071@naver.com>
2021-07-22 22:28:14 +0900

    - 개인공부 String클래스

--- Study/src/class_study/StringExample.java
+++ Study/src/class_study/StringExample.java
@@ -7,21 +7,48 @@ public class StringExample {
 		String name2 = "Kim";
 		String name3 = new String("Kim");
 		
-		System.out.println(System.identityHashCode(name1));
-		System.out.println(System.identityHashCode(name2));
-		System.out.println(System.identityHashCode(name3));
 		
 		if (name1 == name2) {
-			System.out.println(name1 == name2);
+			System.out.println("name 1 과 name2 는 주소가 같다." + true);
 		}else {
-			System.out.println(name1 == name2);
+			System.out.println("name 1 과 name2 는 주소가 다르다." + false);
 		}
 		
 		if (name1 == name3) {
-			System.out.println(name1 == name3);
+			System.out.println("name 1 과 name3 는 주소가 같다." + true);
 		}else {
-			System.out.println(name1 == name3);
+			System.out.println("name 1 과 name2 는 주소가 다르다." + false);
 		}
+
+		if (name1.equals(name2)) {
+			System.out.println("문자열 내용이 같다." + true);
+		}else {
+			System.out.print("문자열 내용이 다르다 " + false);
+		}
+		
+		if (name1.equals(name3)) {
+			System.out.println("문자열 내용이 같다." + true);
+		}else {
+			System.out.print("문자열 내용이 다르다 " + false);
+		}
+		
+		String name4 = new String("커피");
+		String name5 = new String("커피");
+		
+		if(name4.equals(name5))
+			System.out.println(true);
+		else 
+			System.out.println(false);
+		
+		String coffee = "Ediya";
+		coffee = "Kanu";
+		System.out.println(coffee);
+		
+		for (int i = 0 ; i < 10;  i ++) {
+			String test = "안녕하세요?";
+			System.out.println(test);
+		}
+		
 		
 	}
 }
