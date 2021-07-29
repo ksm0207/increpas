be5b42b27265c09c7424d445ad27acce7d394e83
ksm0207 <ksm03071@naver.com>
2021-07-30 08:31:41 +0900

    - 블로그 포스팅용
    - contains() 메소드 활용법

---
+++ Study/src/personal_study/Find.java
@@ -0,0 +1,14 @@
+package personal_study;
+
+public class Find {
+	public static void main(String[] args) {
+		
+		String fruit = "딸기 사과 수박 바나나 메론 복숭아";
+		
+		if (fruit.contains("바나나")) {
+			System.out.println("contains() : 포함");
+		}else {
+			System.out.println("contains() : 포함");
+		}
+	}
+}
