c748acbdbb61b8be403f3877283d11f34fac53b5
ksm0207 <ksm03071@naver.com>
2021-07-31 15:56:25 +0900

    - 개인공부 백준 11719 , 9655

---
+++ Study/src/acmicpc/B11719.java
@@ -0,0 +1,25 @@
+package acmicpc;
+
+import java.util.Scanner;
+
+public class B11719 {
+	public static void main(String[] args) {
+		
+		// 1. 입력 받은 대로 출력하는 프로그램 만들기
+		// 2. 출력
+		
+		//       Hello
+		
+		//Baekjoon
+		//   Online Judge  
+		Scanner scan = new Scanner(System.in);
+		String input = "";
+		while(scan.hasNextLine()) {
+			input = scan.nextLine();
+			System.out.println(input);
+		}
+		
+
+		
+	}
+}
