832ec5c0ae4827b18751681379d5749665680472
ksm0207 <ksm03071@naver.com>
2021-08-13 06:29:34 +0900

    - 스레드 테스트
    - 곱하기

---
+++ Study/src/Thread/Multiplication.java
@@ -0,0 +1,39 @@
+package Thread;
+
+import java.util.Scanner;
+
+public class Multiplication {
+
+	Thread thread;
+	
+	public Multiplication() {
+		
+		Scanner scan =new Scanner(System.in);
+		System.out.print("단 입력 : ");
+		int dan = scan.nextInt();
+		
+		thread = new Thread() {
+			@Override
+			public void run() {
+				try {
+					for (int i=1; i<=9; i++) {
+						System.out.println(dan + " * " + i + " = " + dan * i);
+						sleep(1000);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		
+		};
+		getResult();
+	}
+	
+	public void getResult() {
+		thread.start();
+	}
+	
+	public static void main(String[] args) {
+		 new Multiplication();
+	}
+}
