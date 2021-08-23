3964e6d31a621267101dab72e945855df2440fa3
ksm0207 <ksm03071@naver.com>
2021-08-23 09:07:57 +0900

    - 주소 찾기 복습

--- Study/src/Files/Address.java
+++ Study/src/Files/Address.java
@@ -1,4 +1,4 @@
-package pm;
+package Files;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
@@ -8,7 +8,6 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -16,56 +15,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Vector;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextArea;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.JScrollPane;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
-public class MyAddress extends JFrame {
-
+public class Address extends JFrame {
 	
-	private JPanel contentPane;
+private JPanel contentPane;
 	
-	private JTextField area;
-	private JTextField resText;
+	private JTextField search_text;
+	private JTextField clickText;
 	private JButton confirm;
-	private JScrollPane scrollPane;
+	
 	private JList<String> list;
 	
 	BufferedReader br;
 	FileInputStream fis;
 	String path = "C:/Users/user/Desktop/0820/서울특별시.txt";
 	
-	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) throws Exception {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MyAddress frame = new MyAddress();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public MyAddress() {
-		
-		
+	public Address() {
 		setBounds(100, 100, 415, 488);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -75,28 +49,29 @@ public class MyAddress extends JFrame {
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.NORTH);
 		
-		resText = new JTextField();
-		panel.add(resText);
-		resText.setColumns(10);
+		clickText = new JTextField();
+		panel.add(clickText);
+		clickText.setColumns(10);
 		
 		JLabel label = new JLabel("도로명 :");
 		panel.add(label);
 		
-		area = new JTextField();
-		area.setToolTipText("");
-		panel.add(area);
-		area.setColumns(10);
+		search_text = new JTextField();
+		search_text.setToolTipText("");
+		panel.add(search_text);
+		search_text.setColumns(10);
 		
 		confirm = new JButton("검색");
 		panel.add(confirm);
 		
 		list = new JList();
 		contentPane.add(list, BorderLayout.CENTER);
-	
+		
 		this.addWindowListener(new WindowAdapter() {
 			
 			@Override
 			public void windowClosing(WindowEvent e) {
+				
 				System.exit(0);
 				
 				try {
@@ -120,59 +95,66 @@ public class MyAddress extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-			
-				 String search = area.getText();
-				 String text = "";
-		
-				 System.out.println("검색 : " + search);
-			 
+				
+				String search = search_text.getText();
+				String text = "";
+				Vector<String> vector = new Vector<String>();
+				
+				System.out.println("검색 : " + search);
+				
 				try {
+					
 					fis = new FileInputStream(path);
-					br = new BufferedReader(new InputStreamReader(fis));	
+					br = new BufferedReader(new InputStreamReader(fis));
 					
-					Vector<String> vector = new Vector<String>();
-					JOptionPane.showMessageDialog(MyAddress.this, "검색중...");	
+					JOptionPane.showMessageDialog(Address.this, search + "  검색중...  ");
+					while ((text = br.readLine()) != null ) {	
 						
-					while((text = br.readLine()) != null ) {	
 						if(text.contains(search)) {
-							StringBuffer sb = new StringBuffer();	
+							StringBuffer sb = new StringBuffer();
 							
-							String[] sp =  text.split("\\|");
-							sb.append(sp[0]);
+							String[] sub = text.split("\\|");
+							
+							sb.append(sub[0]);
 							sb.append(":");
-							sb.append(sp[8]);
-							vector.add(sb.toString());
-							area.setText("");
+							sb.append(sub[8]);
+							vector.add(sb.toString());						
 						}
-		
-					}// end of while
-					
+						search_text.setText("");
+						
+					}
 					if (vector.isEmpty()) {
-						JOptionPane.showMessageDialog(MyAddress.this, "검색하신 "+search + " 는 없는 결과 입니다.");	
+						JOptionPane.showMessageDialog(Address.this, search + " 는(은) 없는 결과 입니다.");
 					}else {
-						JOptionPane.showMessageDialog(MyAddress.this, search + " 결과 입니다. ");	
+						JOptionPane.showMessageDialog(Address.this, search + " 검색 결과 입니다.");
 						list.setListData(vector);
 					}
 				} catch (Exception e1) {
+					// TODO Auto-generated catch block
 					e1.printStackTrace();
-				}			
+				}
 			}
 		});
-		
 		list.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mousePressed(MouseEvent e) {
+			public void mouseClicked(MouseEvent e) {
 				
-				int click = e.getClickCount();
-				
-				if (click == 2) {
-					String get = list.getSelectedValue();
-					
-					String[] res = get.split(":");
-					resText.setText(res[0]);	
+				String firstCode = list.getSelectedValue();
+				clickText.setText(firstCode.substring(0,5));
+			}
+		});
+	}
+
+	public static void main(String[] args) throws Exception {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Address frame = new Address();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 			}
 		});
 	}
-	
 }
